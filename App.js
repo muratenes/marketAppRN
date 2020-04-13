41987f9d7ffbcc3177f217e81fbe373306163b2c
muratenes <m4urat@gmail.com>
2020-04-13 11:13:38 +0300

    firebase notifitacon worked

--- App.js
+++ App.js
@@ -1,55 +1,72 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
-import Router from './src/Router'
-import {Provider} from 'mobx-react';
-import store from "./src/store/";
-import NavigationService from "./src/NavigationService";
+import {StyleSheet, Text, View, Button, PushNotificationIOS} from 'react-native';
+
 import PushNotification from 'react-native-push-notification';
+import firebase from 'react-native-firebase';
+import {Provider} from "mobx-react";
+import Router from "./src/Router";
+import NavigationService from "./src/NavigationService";
+import store from '../movieApp/src/store/';
+
 
 export default class App extends Component {
-    componentDidMount(): void {
-
-    }
-
-    sendNotification() {
-        PushNotification.localNotification({
-            /* iOS and Android properties */
-            title: "My Notification Title", // ( optional)
-            message: "My Notification Message", // (required)
-        });
-    }
-
     render() {
         return (
-            <View>
-                <Button title={'tikla'} onPress={this.sendNotification}/>
-            </View>
-
-            // <Provider {...store}>
-            //     <Router
-            //         ref={navigatorRef => {
-            //             NavigationService.setTopLevelNavigator(navigatorRef);
-            //         }}
-            //     />
-            // </Provider>
+            <Provider {...store}>
+                <Router
+                    ref={navigatorRef => {
+                        NavigationService.setTopLevelNavigator(navigatorRef);
+                    }}
+                />
+            </Provider>
         );
     }
 }
 
+const messaging = firebase.messaging();
+
+messaging.hasPermission()
+    .then((enabled) => {
+        if (enabled) {
+            messaging.getToken()
+                .then(token => {
+                    console.log(token)
+                })
+                .catch(error => { /* handle error */
+                });
+        } else {
+            messaging.requestPermission()
+                .then(() => { /* got permission */
+                })
+                .catch(error => { /* handle error */
+                });
+        }
+    })
+    .catch(error => { /* handle error */
+    });
+
+firebase.notifications().onNotification((notification) => {
+    const {title, body} = notification;
+    PushNotification.localNotification({
+        title: title,
+        message: body, // (required)
+    });
+});
+
 PushNotification.configure({
+
     // (optional) Called when Token is generated (iOS and Android)
     onRegister: function (token) {
-        console.log("TOKEN:", token);
-        console.warn("token",token)
+        console.log('TOKEN:', token);
     },
 
     // (required) Called when a remote or local notification is opened or received
     onNotification: function (notification) {
-        console.log("NOTIFICATION:", notification);
+        console.log('NOTIFICATION:', notification);
 
         // process the notification
 
-        // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
+        // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
         notification.finish(PushNotificationIOS.FetchResult.NoData);
     },
 
@@ -72,7 +89,5 @@ PushNotification.configure({
      * - Specified if permissions (ios) and token (android and ios) will requested or not,
      * - if not, you must call PushNotificationsHandler.requestPermissions() later
      */
-    requestPermissions: true
+    requestPermissions: true,
 });
-
-const styles = StyleSheet.create({});
